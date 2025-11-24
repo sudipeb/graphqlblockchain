bb3906e1634f0199e978ae604ad0ca087df4bffc
sudipeb <sudip.koirala@ebpearls.com>
2025-11-24 17:48:05 +0545

    added the apikey for the bitquery

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,3 +1,4 @@
-class ApiKey{
-  static const 
+class ApiKey {
+  static const String bitQueryApiKey =
+      ' ory_at_6ev9RMIEfWXTgXp1oqbuMpSrTf5nOqcRtq-IiC6Fb30.97tWL-wvz3gECsEmS8updXJXcgqvm8aB8uCg_X3A4NI';
 }

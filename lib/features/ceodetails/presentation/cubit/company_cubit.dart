8a671605fa4ea72e07b7cce69a935c5c7a1359b8
sudipeb <sudip.koirala@ebpearls.com>
2025-11-25 15:59:53 +0545

    created the cubit

---
+++ lib/features/ceodetails/presentation/cubit/company_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graphbitcoin/features/ceodetails/domain/entity/company_entity.dart';
+import 'package:graphbitcoin/features/ceodetails/domain/repository/company_repo_impl.dart';
+
+abstract class CompanyState {}
+
+class CompanyInitial extends CompanyState {}
+
+class CompanyLoading extends CompanyState {}
+
+class CompanyLoaded extends CompanyState {
+  final Company company;
+  CompanyLoaded(this.company);
+}
+
+class CompanyError extends CompanyState {
+  final String message;
+  CompanyError(this.message);
+}
+
+class CompanyCubit extends Cubit<CompanyState> {
+  final CompanyRepositoryImpl repository;
+  CompanyCubit(this.repository) : super(CompanyInitial());
+
+  Future<void> fetchCompany() async {
+    emit(CompanyLoading());
+    try {
+      final company = await repository.getCompany();
+      emit(CompanyLoaded(company));
+    } catch (e) {
+      emit(CompanyError(e.toString()));
+    }
+  }
+}

93749462afeeb5782f85880be7ba72a2970322e4
sudipeb <sudip.koirala@ebpearls.com>
2025-11-28 10:31:12 +0545

    cubit to handle the history state introduced

--- lib/features/history/presentation/cubit/history_cubit.dart
+++ lib/features/history/presentation/cubit/history_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphbitcoin/features/history/domain/entity/history.dart';
 
 abstract class HistoryState {}
@@ -14,4 +15,19 @@ class HistoryLoaded extends HistoryState {
 class HistoryError extends HistoryState {
   final String message;
   HistoryError(this.message);
+}
+
+class HistoryCubit extends Cubit<HistoryState> {
+  final HistoryRepository repository;
+  HistoryCubit(this.repository) : super(HistoryInitial());
+
+  Future<void> FetchHistory() async {
+    emit(HistoryLoading());
+    try {
+      final history = await repository.getHistory();
+      emit(HistoryLoaded(history));
+    } catch (e) {
+      emit(HistoryError(e.toString()));
+    }
+  }
 }

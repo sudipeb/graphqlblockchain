885163d9545b0a24eb7bf8169e2d349b3b9bded2
sudipeb <sudip.koirala@ebpearls.com>
2025-11-28 10:15:10 +0545

    created the remote data source dependency injection

--- lib/core/dependencyinjection/di.dart
+++ lib/core/dependencyinjection/di.dart
@@ -4,6 +4,7 @@ import 'package:graphbitcoin/core/services/graphql_service.dart';
 import 'package:graphbitcoin/features/ceodetails/data/datasources/company_remote_datasource.dart';
 import 'package:graphbitcoin/features/ceodetails/domain/repository/company_repo_impl.dart';
 import 'package:graphbitcoin/features/ceodetails/presentation/cubit/company_cubit.dart';
+import 'package:graphbitcoin/features/history/data/datasource/history_remote_datasource.dart';
 
 final GetIt dep = GetIt.instance;
 void setupDependencies() {
@@ -12,15 +13,17 @@ void setupDependencies() {
 
   // Data sources
   dep.registerSingleton<CompanyRemoteDataSource>(
-    CompanyRemoteDataSourceImpl(graphqlService: dep<GraphQLService>())
+    CompanyRemoteDataSourceImpl(graphqlService: dep<GraphQLService>()),
+  );
+  dep.registerSingleton<HistoryRemoteDataSource>(
+    HistoryRemoteDataSourceImpl(graphqlService: dep<GraphQLService>()),
   );
 
   // Repositories
   dep.registerSingleton<CompanyRepository>(
-    CompanyRepositoryImpl(remoteDataSource: dep<CompanyRemoteDataSource>())
+    CompanyRepositoryImpl(remoteDataSource: dep<CompanyRemoteDataSource>()),
   );
 
   // Cubits
   dep.registerFactory(() => CompanyCubit(dep<CompanyRepository>()));
 }
-

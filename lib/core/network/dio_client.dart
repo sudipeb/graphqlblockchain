ad2d9a07cb41761d5a7a7cf3e849775c33e38b3a
sudipeb <sudip.koirala@ebpearls.com>
2025-11-28 13:33:50 +0545

    changed the comment

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:graphbitcoin/core/config/app_config.dart';
 
+/// A singleton class to manage [Dio] HTTP client
 class DioClient {
   late final Dio dio;
   DioClient._internal() {
@@ -11,10 +12,12 @@ class DioClient {
         validateStatus: (status) => status != null && status < 500,
       ),
     );
+    //logging interceptor to print request and response details
     dio.interceptors.add(LogInterceptor(requestBody: true));
   }
 
   static final DioClient _instance = DioClient._internal();
 
+  /// Factory constructor to return the singleton instance
   factory DioClient() => _instance;
 }

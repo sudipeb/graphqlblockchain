0e0738dc6310861a04bf7064acde4ea1362230bd
sudipeb <sudip.koirala@ebpearls.com>
2025-12-03 10:41:07 +0545

    changed the history view

--- lib/features/history/presentation/pages/history_page.dart
+++ lib/features/history/presentation/pages/history_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphbitcoin/core/dependencyinjection/di.dart';
 import 'package:graphbitcoin/features/history/presentation/blocs/history_cubit.dart';
-import 'package:graphbitcoin/features/history/presentation/pages/history_vieww.dart';
+import 'package:graphbitcoin/features/history/presentation/pages/history_view.dart';
 
 @RoutePage()
 class HistoryPage extends StatelessWidget {
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,7 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:graphbitcoin/features/ceodetails/presentation/pages/company_page.dart';
 import 'package:graphbitcoin/features/history/presentation/pages/history_page.dart';
-import 'package:graphbitcoin/features/history/presentation/pages/history_vieww.dart';
+import 'package:graphbitcoin/features/history/presentation/pages/history_view.dart';
+
 import 'package:graphbitcoin/features/rocket/presentation/pages/rocket_page.dart';
 import 'package:graphbitcoin/features/rocket/presentation/pages/rocket_view.dart';
 part 'app_router.gr.dart';
